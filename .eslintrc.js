771d460abc6543e768772551b94cf2397bc4e267
AndersonVanzo <anderson.vanzo01@gmail.com>
2024-02-18 01:10:34 -0300

    chore: add prettier and eslint configurations

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    root: true,
+    extends: "@react-native",
+    rules: {
+        quotes: ["error", "double"],
+        "react/react-in-jsx-scope": ["off"],
+        "react-hooks/exhaustive-deps": ["off"],
+    },
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
-const MusicLibrary = require('./NativeMusicLibrary').default;
+const MusicLibrary = require("./NativeMusicLibrary").default;
 
 export function multiply(a: number, b: number): number {
-  return MusicLibrary.multiply(a, b);
+    return MusicLibrary.multiply(a, b);
 }
